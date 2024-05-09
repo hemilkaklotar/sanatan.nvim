98509a882e9daad808c530bf4c92f93f2c86e3a2
Hemil Kaklotar <83568973+hemilkaklotar@users.noreply.github.com>
2024-05-09 16:01:45 +0530

    Added plugin and updated base config

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,26 +1,32 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    css = { "prettierd" },
-    html = { "prettierd" },
-    javascript = { "prettierd" },
-    javascriptreact = { "prettierd" },
-    json = { "prettierd" },
-    jsonc = { "prettierd" },
-    markdown = { "prettierd" },
-    typescript = { "prettierd" },
-    typescriptreact = { "prettierd" },
-    yaml = { "prettierd" },
+    css = { { "prettierd", "prettier" } },
+    html = { { "prettierd", "prettier" } },
+    javascript = { { "prettierd", "prettier" } },
+    javascriptreact = { { "prettierd", "prettier" } },
+    json = { { "prettierd", "prettier" } },
+    jsonc = { { "prettierd", "prettier" } },
+    markdown = { { "prettierd", "prettier" } },
+    typescript = { { "prettierd", "prettier" } },
+    typescriptreact = { { "prettierd", "prettier" } },
+    yaml = { { "prettierd", "prettier" } },
     python = { "isort", "black" },
     go = { "gofmt" },
   },
-
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-    asycn = false,
-  },
+  format_on_save = function(bufnr)
+    -- Disable "format_on_save lsp_fallback" for languages that don't
+    -- have a well standardized coding style. You can add additional
+    -- languages here or re-enable it for the disabled ones.
+    local disable_filetypes = { c = true, cpp = true }
+    return {
+      -- These options will be passed to conform.format()
+      timeout_ms = 500,
+      -- lsp_fallback = true,
+      lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+      asycn = false,
+    }
+  end,
 }
 
 require("conform").setup(options)
