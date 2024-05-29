0bbd754499d35d191ed87023611e25ba146597f9
Hemil Kaklotar <83568973+hemilkaklotar@users.noreply.github.com>
2024-05-29 07:20:35 +0000

    updated default theme

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -9,8 +9,8 @@ M.ui = {
   hl_add = {},
   hl_override = {},
   changed_themes = {},
-  theme_toggle = { "onenord", "one_light" },
-  theme = "onenord", -- default theme
+  theme_toggle = { "onedark", "one_light" },
+  theme = "onedark", -- default theme
   transparency = true,
 
   cmp = {
